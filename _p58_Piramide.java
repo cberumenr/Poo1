74bbc83e0401a302702f6cab949997cbc81e32cc
cberumenr <37186747@uaz.edu.mx>
2023-09-17 01:08:30 -0600

    Actividad 10

--- _p58_Piramide.java
+++ _p58_Piramide.java
@@ -9,17 +9,17 @@ public class _p58_Piramide {
         int i, j, n;
         char car, resp;
         Scanner obj = new Scanner(System.in);
-        
+
         do {
             System.out.print("\033[H\033[2J"); System.out.flush();
             System.out.print("Cuantos renglones ? ");
-            
+
             n = obj.nextInt();
-            
+
             System.out.print("De que caracter ? ");
-            
+
             car = obj.next().charAt(0);
-            
+
             for (i = 1; i <= n; i++) {
                 for (j = 1; j <= i; j++) {
 
@@ -28,11 +28,11 @@ public class _p58_Piramide {
                 }
                 System.out.println();
             }
-            
+
             System.out.print("\nDeseas continuar (S/N) ?");
             resp = Character.toUpperCase(obj.next().charAt(0));
         } while (resp != 'N');
-        
+
         System.out.println("\nProceso terminado ..");
     }
 }
