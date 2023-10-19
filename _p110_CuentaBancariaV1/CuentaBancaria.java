9752fc1dc8c4ef38777468d4ae07cac0600004c6
cberumenr <37186747@uaz.edu.mx>
2023-10-19 12:57:48 -0600

    Actividad 18

---
+++ _p110_CuentaBancariaV1/CuentaBancaria.java
@@ -0,0 +1,30 @@
+package _p110_CuentaBancariaV1;
+
+public class CuentaBancaria {
+    private double Saldo;
+
+    public CuentaBancaria() {
+    }
+
+    public CuentaBancaria(double cantidad) {
+        Saldo = cantidad;
+    }
+
+    public void deposita(double cantidad) {
+        Saldo += cantidad;
+    }
+
+    public double getSaldo() {
+        return Saldo;
+    }
+
+    public boolean retira(double cantidad) {
+        if(Saldo >= cantidad) {
+            Saldo -= cantidad;
+            return true;
+        }
+        else
+            return false;
+    }
+    
+}
